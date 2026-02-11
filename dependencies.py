f9a05568a5c9840c5f3853217bfb0d1d52b914b0
alexandre <alexandre020602@gmail.com>
2026-02-11 14:59:03 -0300

    Session

---
+++ dependencies.py
@@ -0,0 +1,11 @@
+from sqlalchemy.orm import sessionmaker
+from models import db
+
+def get_session ():
+    try:
+        session =  sessionmaker(bind=db)
+        session = session()
+        yield session
+
+    finally:
+        session.close()
