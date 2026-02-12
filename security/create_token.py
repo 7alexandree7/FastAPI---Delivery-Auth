1fbb717d7b686be28084ef9103da20f89a590e0e
alexandre <alexandre020602@gmail.com>
2026-02-12 20:15:11 -0300

    add tipagem

--- security/create_token.py
+++ security/create_token.py
@@ -5,6 +5,6 @@ from main import ACCESS_TOKEN_EXPIRES_MINUTES, SECRET_KEY, ALGORITHM
 
 def create_token(id_user, duration_token=timedelta(minutes=ACCESS_TOKEN_EXPIRES_MINUTES)):
     expiration_date = datetime.now(timezone.utc) + duration_token
-    dic_info = { "sub": id_user, "exp": expiration_date }
+    dic_info = { "sub": str(id_user), "exp": expiration_date }
     encoded_jwt = jwt.encode(dic_info, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
