c68730d5564eed9bbc73d2dc2dde96c97485c029
alexandre <alexandre020602@gmail.com>
2026-02-12 14:26:12 -0300

    inicializando a função de verify_token

--- security/jwt.py
+++ security/jwt.py
@@ -1,15 +1,22 @@
-from fastapi import HTTPException
+from fastapi import HTTPException, Depends
 from models.models import User
 from main import bcrypt_context, ACCESS_TOKEN_EXPIRES_MINUTES, SECRET_KEY, ALGORITHM
 from jose import jwt
 from datetime import datetime, timedelta, timezone
+from sqlalchemy.orm import Session
+from db.session import get_session
 
 
-def create_token(id_user):
-    expiration_date = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRES_MINUTES)
+def create_token(id_user, duration_token=timedelta(minutes=ACCESS_TOKEN_EXPIRES_MINUTES)):
+    expiration_date = datetime.now(timezone.utc) + duration_token
     dic_info = { "sub": id_user, "exp": expiration_date }
     encoded_jwt = jwt.encode(dic_info, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
+
+
+def verify_token(token, session: Session = Depends(get_session)):
+    user = session.query(User).filter(User.id == 1).first()
+    return user
 
 
 
