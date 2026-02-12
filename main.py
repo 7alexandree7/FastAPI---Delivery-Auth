eb0c1b9e7914eacf38aa10ea8184fc5142daeea2
alexandre <alexandre020602@gmail.com>
2026-02-12 20:13:03 -0300

    add oauth2

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from dotenv import load_dotenv
 from passlib.context import CryptContext
+from fastapi.security import OAuth2PasswordBearer
 import os
 
 load_dotenv()
@@ -12,6 +13,7 @@ ACCESS_TOKEN_EXPIRES_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRES_MINUTES"))
 app = FastAPI()
 
 bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/login-form")
 
 from routes.auth_routes import auth_route
 from routes.order_routes import order_route
